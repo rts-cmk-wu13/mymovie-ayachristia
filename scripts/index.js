6109f63298017ac478290073f8471cb0a27a4d56
iamayac <ayachristia@gmail.com>
2025-03-04 21:34:52 +0100

    genre fetch correct

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,10 @@
 "use strict";
 let mainContent = document.querySelector(".main");
 
-//------------endpoint-------------movie lists: now playing-----------horisontal scroll
+//movie lists: now playing api-----------horisontal scroll
 const playingUrl =
   "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1";
-const playingOptions = {
+const options = {
   method: "GET",
   headers: {
     accept: "application/json",
@@ -14,7 +14,7 @@ const playingOptions = {
   },
 };
 
-fetch(playingUrl, playingOptions)
+fetch(playingUrl, options)
   .then((res) => {
     if (res.status === 200) {
       return res.json();
@@ -22,22 +22,57 @@ fetch(playingUrl, playingOptions)
       throw new Error("nothing found");
     }
   })
-  .then((json) => console.log(json))
+  .then((movies) => {
+    let moviesArray = movies.results;
+    console.log(moviesArray);
+    let listContainerShowing = document.querySelector(".index__showing-list");
+    listContainerShowing.innerHTML += moviesArray
+      .map((movie) => {
+        return `
+        <a href="details.html?id=${movie.id}">
+        <article class="index__showing-movie">
+          <div class="index__showing-imgContainer">
+            <img src="https://image.tmdb.org/t/p/w500${
+              movie.poster_path
+            }" alt="${movie.title}" class="index__showing-img">
+          </div>
+          <h2 class="index__showing-titel">${movie.title}</h2>
+          <p class="index__showing-rating"><span class="material-symbols-outlined">star</span> ${movie.vote_average.toFixed(
+            1
+          )}/10 IMDb</p>
+        </article>
+      </a>
+        `;
+      })
+      .join("");
+  })
   .catch((err) => console.error(err));
 
-//---------------endpoint------------movie lists: popular----------------fortsætter ned ad siden/intersectionobserver
+//movie lists: popular api----------------fortsætter ned ad siden/intersectionobserver
+// --------------- genres api
+
+let genreList = [];
+
+const genreUrl = "https://api.themoviedb.org/3/genre/movie/list?language=en";
+fetch(genreUrl, options)
+  .then((res) => res.json())
+  .then((data) => {
+    data.genres.forEach((genre) => {
+      genreList.push(genre);
+    });
+  })
+  .catch((err) => console.error(err));
+
+function getGenreNames(ids) {
+  return ids.map((id) => {
+    return genreList.find((genre) => genre.id == id).name;
+  });
+}
+// -------------popular api
 const popularUrl =
   "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
-const popularOptions = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZmIyZDVhZDZjMTBkZGYwZjk0N2Q2NWFlNWRlODljYyIsIm5iZiI6MTc0MTAwMjQ2OS4wMTksInN1YiI6IjY3YzU5NmU1YzdjYWJhNDI0YzkxZmU0MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.syFixX53XmNC4Ivc4Eci2Wma89qYRuCZKKQdrBDhCpQ",
-  },
-};
 
-fetch(popularUrl, popularOptions)
+fetch(popularUrl, options)
   .then((res) => {
     if (res.status === 200) {
       return res.json();
@@ -45,5 +80,44 @@ fetch(popularUrl, popularOptions)
       throw new Error("nothing found");
     }
   })
-  .then((json) => console.log(json))
+  .then((popular) => {
+    let popularArray = popular.results;
+    console.log(popularArray);
+
+    let listContainerPopular = document.querySelector(".index__popular-list");
+    listContainerPopular.innerHTML += popularArray
+      .map((popular) => {
+        console.log(popular);
+        // -----------------the DOM
+        return `
+        <a href="details.html?id=${popular.id}">
+      <article class="index__popular-movie">
+        <div class="index__popular-imgContainer">
+          <img src="https://image.tmdb.org/t/p/w500${
+            popular.poster_path
+          }" alt="${popular.title}" class="index__popular-img">
+        </div>
+        <section class="index__popular-details">
+          <h2 class="index__popular-headline">${popular.title}</h2>
+          <p class="index__popular-rating"><span class="material-symbols-outlined">star</span> ${popular.vote_average.toFixed(
+            1
+          )}/10</p>
+          <div class="index__popular-genres">
+            ${getGenreNames(popular.genre_ids)}
+          </div>
+        </section>
+      </article>
+    </a>
+      `;
+      })
+      .join(" ");
+  })
+  .catch((err) => console.error(err));
+
+// -----------------configuration details api
+const configurationUrl = "https://api.themoviedb.org/3/configuration";
+
+fetch(configurationUrl, options)
+  .then((res) => res.json())
+  .then((json) => {})
   .catch((err) => console.error(err));
--- scripts/indexlayout.js
+++ scripts/indexlayout.js
@@ -18,6 +18,22 @@ indexMain.classList.add("index__main");
 main.appendChild(indexMain);
 
 indexMain.innerHTML = `
-<section class="index__showing"></section>
-<section class="index__popular"></section>
+<section class="index__showing">
+  <header class="index__showing-header">
+  <h2>Now showing</h2>
+  <button class="showmore__btns">See more</button>
+  </header>
+  <section class="index__showing-list"></section>
+</section>
+
+<section class="index__popular">
+  <header class="index__popular-header">
+  <h2>Popular</h2>
+  <button class="showmore__btns">See more</button>
+  </header>
+  
+<section class="index__popular-list">
+</section>
+
+</section>
 `;
