6a6b774078817d1578a6e1be7e0f85eced8ed5af
iamayac <ayachristia@gmail.com>
2025-03-10 22:14:02 +0100

    rode fil

--- scripts/rod.js
+++ scripts/rod.js
@@ -13,3 +13,19 @@ releasesMovie.release_dates.forEach((release) => {
 });
 // console.log(certification);
 //returnere et array
+
+releaseCountries = data.results;
+originCountry = "US";
+
+console.log(releaseCountries);
+let releaseForMovieCountry = releaseCountries.find(
+  (releaseInfo) => releaseInfo.iso_3166_1 == originCountry
+).release_dates[0].certification;
+console.log(releaseForMovieCountry);
+// let releaseForMovieCountry = releaseCountries.find(
+//   (releaseInfo) => releaseInfo.iso_3166_1 == originCountry
+// ).release_dates[0].certification;
+// console.log(releaseForMovieCountry);
+
+let moviePG = document.querySelector(".details__PG");
+moviePG.textContent = `${releaseForMovieCountry}`;
