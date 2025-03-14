32a3a16ea6eb189b73976f1348a1e60fb58b4e0a
iamayac <ayachristia@gmail.com>
2025-03-14 13:13:19 +0100

    wai-aria, burgermenu

--- scripts/burgermenu.js
+++ scripts/burgermenu.js
@@ -1,21 +1,24 @@
 let headerLeft = document.querySelector(".header__left");
 headerLeft.innerHTML = `
 
-<div class="burger__nav">
-    <button class="burger__nav-btn">
+<div class="burger__nav" role="navigation">
+    <button class="burger__nav-btn" aria-controles="primaryNavigation" aria-describedby="message-menu-button" aria-expanded="false" aria-label="menuButton">
     <span class="material-symbols-outlined">
 menu
 </span>
 </button>
  
 
- <nav class="burger__nav-list hidden">
-    <a href="index.html">Main page</a>
-    <a href="favorites.html">Favorites</a>
-    <button class="burger__nav-close">X</button>
+ <nav class="burger__nav-list hidden" id="primaryNavigation" role="navigation">
+    <a href="index.html" aria-label="goToMainPage">Main page</a>
+    <a href="favorites.html" aria-label="goToFavorites">Favorites</a>
+    <button class="burger__nav-close" aria-label="closeMenuButton">X</button>
  </nav>
   </div>
   <div class="burger__nav-overlay hidden"></div>
+  <div hidden>
+    <span id="message-menu-button">Klik her for at vise menuen</span>
+  </div>
 `;
 
 const overlay = document.querySelector(".burger__nav-overlay");
@@ -25,10 +28,13 @@ const navList = document.querySelector(".burger__nav-list");
 burgerBtn.addEventListener("click", function () {
   navList.classList.remove("hidden");
   overlay.classList.remove("hidden");
+
+  burgerBtn.setAttribute("aria-expanded", true);
 });
 
 const burgerCloseBtn = document.querySelector(".burger__nav-close");
 burgerCloseBtn.addEventListener("click", function () {
   navList.classList.add("hidden");
   overlay.classList.add("hidden");
+  burgerBtn.setAttribute("aria-expanded", false);
 });
